543ed7a80ded95dd29220aaaf1cdc4e8c2296158
AIrvenRichardson <airvenrichardson@aol.com>
2026-01-07 16:14:35 -0700

    modified the overall structure of the application to include tabs, made a second page for searching entries

--- application.py
+++ application.py
@@ -6,27 +6,57 @@ import sys, datetime
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Add Application Record")
-
-        self.eName = QtWidgets.QLineEdit("Company Name")
-        self.eTitle = QtWidgets.QLineEdit("Role Title")
-        self.eURL = QtWidgets.QLineEdit("URL")
-
-        self.button = QtWidgets.QPushButton("Record")
-
-        
-        self.layout = QtWidgets.QGridLayout(self)
-        self.layout.addWidget(self.eName, 0, 0)
-        self.layout.addWidget(self.eTitle, 1, 0)
-        self.layout.addWidget(self.eURL, 2, 0)
-        self.layout.addWidget(self.button, 1, 3)
-
-        #Database Connection
+        # Database Connection
         self.con = sqlite3.connect("applications.db")
         self.cur = self.con.cursor()
         self.cur.execute("CREATE TABLE IF NOT EXISTS applications(company text, title text, date text, url text, status text)")
 
-        self.button.clicked.connect(self.addRecord)
+
+        # Add Record Window
+        self.t1 = QtWidgets.QWidget() # Blank widget that siomply holds a layout
+        
+        self.eName = QtWidgets.QLineEdit("Company Name")
+        self.eTitle = QtWidgets.QLineEdit("Role Title")
+        self.eURL = QtWidgets.QLineEdit("URL")
+        
+        self.button1 = QtWidgets.QPushButton("Record")
+        self.button1.clicked.connect(self.addRecord)
+ 
+        self.layout1 = QtWidgets.QGridLayout(self)
+        self.layout1.addWidget(self.eName, 0, 0)
+        self.layout1.addWidget(self.eTitle, 1, 0)
+        self.layout1.addWidget(self.eURL, 2, 0)
+        self.layout1.addWidget(self.button1, 1, 3)
+
+        self.t1.setLayout(self.layout1)
+
+        # Update Window
+        self.t2 = QtWidgets.QWidget()
+
+        self.eSearch = QtWidgets.QLineEdit("Company Name Here")
+        self.retrievedEntries = QtWidgets.QLabel("None", alignment=QtCore.Qt.AlignCenter)
+        
+        self.button2 = QtWidgets.QPushButton("Search")
+        self.button2.clicked.connect(self.findRecord)
+
+        self.layout2 = QtWidgets.QVBoxLayout(self)
+        self.layout2.addWidget(self.eSearch)
+        self.layout2.addWidget(self.button2)
+        self.layout2.addWidget(self.retrievedEntries)
+
+        self.t2.setLayout(self.layout2)
+
+        # Main Setup
+        self.setWindowTitle("ApplicationHome")
+        self.tabs = QtWidgets.QTabWidget()
+        self.tabs.addTab(self.t1, "Add Record")
+        self.tabs.addTab(self.t2, "Update Record")
+     
+        self.mainlayout = QtWidgets.QHBoxLayout(self)
+        self.mainlayout.setContentsMargins(0,0,0,0)
+        self.mainlayout.addWidget(self.tabs)
+        self.tabs.show()
+
     
     @QtCore.Slot()
     def addRecord(self):
@@ -37,7 +67,18 @@ class MyWidget(QtWidgets.QWidget):
                 ('{self.eName.text()}', '{self.eTitle.text()}', '{x.strftime('%Y-%m-%d')}', '{self.eURL.text()}', 'not yet')
                 """)
     
+        # Clear out the boxes so you know it's gone through
+        self.eName.setText('')
+        self.eTitle.setText('')
+        self.eURL.setText('')
         self.con.commit()
+    
+    @QtCore.Slot()
+    def findRecord(self):
+        self.res = self.cur.execute(f"""
+                         Select * FROM applications WHERE company LIKE '%{self.eSearch.text()}%'
+                         """)
+        self.retrievedEntries.setText(str(self.res.fetchone()))
 
 
 if __name__ == "__main__":
@@ -47,9 +88,6 @@ if __name__ == "__main__":
     widget = MyWidget()
     widget.resize(600, 200)
     widget.setAutoFillBackground(True)
-    palette = widget.palette()
-    palette.setColor(QPalette.ColorRole.Window, QColor("gray"))
-    widget.setPalette(palette)
     widget.show()
 
     sys.exit(app.exec())
