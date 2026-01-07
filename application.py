2a03e68af29f186b820f32c6bf67e3179be8264e
AIrvenRichardson <airvenrichardson@aol.com>
2026-01-07 12:59:07 -0700

    Can add the the database now, also removed db from git

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
 from PySide6 import QtWidgets, QtCore, QtGui
+from PySide6.QtGui import QPalette, QColor
+import sqlite3
 import sys, datetime
 
 class MyWidget(QtWidgets.QWidget):
@@ -9,9 +11,8 @@ class MyWidget(QtWidgets.QWidget):
         self.eName = QtWidgets.QLineEdit("Company Name")
         self.eTitle = QtWidgets.QLineEdit("Role Title")
         self.eURL = QtWidgets.QLineEdit("URL")
-        self.hello = "hoi"
 
-        self.button = QtWidgets.QPushButton("Click me!")
+        self.button = QtWidgets.QPushButton("Record")
 
         
         self.layout = QtWidgets.QGridLayout(self)
@@ -20,21 +21,35 @@ class MyWidget(QtWidgets.QWidget):
         self.layout.addWidget(self.eURL, 2, 0)
         self.layout.addWidget(self.button, 1, 3)
 
-        self.button.clicked.connect(self.magic)
+        #Database Connection
+        self.con = sqlite3.connect("applications.db")
+        self.cur = self.con.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS applications(company text, title text, date text, url text, status text)")
+
+        self.button.clicked.connect(self.addRecord)
     
     @QtCore.Slot()
-    def magic(self):
+    def addRecord(self):
         x = datetime.datetime.now()
-        print(self.eName.text(), self.eTitle.text(), x.strftime("%x"), self.eURL.text())
 
-
+        self.cur.execute(f"""
+                INSERT INTO applications VALUES
+                ('{self.eName.text()}', '{self.eTitle.text()}', '{x.strftime('%Y-%m-%d')}', '{self.eURL.text()}', 'not yet')
+                """)
+    
+        self.con.commit()
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
+    # Configure The window/widget with size and BG
     widget = MyWidget()
     widget.resize(600, 200)
+    widget.setAutoFillBackground(True)
+    palette = widget.palette()
+    palette.setColor(QPalette.ColorRole.Window, QColor("gray"))
+    widget.setPalette(palette)
     widget.show()
 
     sys.exit(app.exec())
